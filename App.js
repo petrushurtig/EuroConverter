e70929b9b4dd835189ec137ec5f704d3fb016f1d
Petrus Hurtig <petrushurtig@hotmail.com>
2021-01-23 19:09:05 +0200

    ainitial commit

--- App.js
+++ App.js
@@ -4,25 +4,22 @@ import { StyleSheet, Text, View, Alert, Image, TextInput,Button } from 'react-na
 import {Picker} from '@react-native-picker/picker';
 
 export default function App() {
-const [code, setCode] = useState();
+const [selected, setSelected] = useState();
 const [rates, setRates] = useState([]);
 const [amount, setAmount] = useState('');
 const [keys, setKeys] = useState([]);
 const [result, setResult] = useState('');
 
-//React.useEffect(() => getRates(),[]);
 
 const getRates= () => {
 const access_key = '77b1f22fa980fbd7adeebc5c29cf4154';
-const url = 'http://data.fixer.io/api/convert?access_key=' + access_key + 
-'&from=' + code + '&to=EUR&amount=' + amount;
+const url = 'http://data.fixer.io/api/latest?access_key=' + access_key;
   fetch(url)
   .then((res) => res.json())
   .then((data) => {
     setRates(data.rates);
     console.log(url)
     setKeys(Object.keys(data.rates))
-
   })
 .catch((error) => {
   Alert.alert('Error', error.message);
@@ -35,24 +32,32 @@ keys.map((s, i) => {
     //return <Picker.Item key={s} value={i} label={i} />
   
 });
+React.useEffect(() => getRates(),[]);
 
+const convert = () => {
+  const amountEur = Number(amount) / rates[selected];
+  setResult(`${amountEur.toFixed(2)}€`);
+}
 
   return (
     <View style={styles.container}>
       <Image style={{height:100, width:150}} source={{uri:'https://preview.pixlr.com/images/800wm/1195/1/11951502686.jpg'}}></Image>
-      <Text>{result} €</Text>
+      <Text>{result} </Text>
+      <View style={{ flexDirection: 'row'}}> 
       <TextInput 
       keyboardType="number-pad"
       value={amount}
       onChangeText={(val) => setAmount(val)}
-      style={{borderWidth:1, borderColor:'black', width:50}}></TextInput>
+      style={{ borderBottomWidth:1, borderColor:'blue', width:70, height:30}}></TextInput>
      <Picker 
+     selectedValue={selected}
       style={{height: 50, width: 200}}
-      onValueChange={(val) => setCode(val)}>
-     
+      onValueChange={(itemValue, ItemIndex) => setSelected(itemValue)}>
     
+         {codes}
       </Picker> 
-      <Button onPress={getRates} title="Convert"></Button>
+      </View>
+      <Button onPress={convert} title="Convert"></Button>
       <StatusBar style="auto" />
     </View>
   );
